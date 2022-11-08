5b0df36496b47ff9472ad83d8e23f25ec5f5345d
yudiz-sahibe <sahibe.alam@yudiz.com>
2022-11-08 11:55:16 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
      <div className="contact">
   <div style={{maxWidth: '400px'}} classNameName='brand'>
     <img style={{width: '90%'}} src={logo} alt="" />
-    <p>hello</p>
+    <h2>hello yudiz</h2>
   </div>
   <div className="contact-form">
     <label for="name">
